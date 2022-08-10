80542624d6e8c0544a784042613e8773975207de
Danny Gilbert <dgilbert@foreflight.com>
2022-08-10 15:48:28 -0500

    Support token caching / retries

--- Sources/Shuttle/JWT/JWTRequestsAuthenticator.swift
+++ Sources/Shuttle/JWT/JWTRequestsAuthenticator.swift
@@ -6,12 +6,27 @@
 import Foundation
 import Get
 
-final class JWTRequestsAuthenticator {
-
+public final class JWTRequestsAuthenticator {
+    
     /// The JWT Token to use for creating requests. Can be overriden for test use cases.
-    var provider: JWTToken
-
-    init(_ token: JWTToken) {
+    private let provider: JWTToken
+    
+    private(set) var cached_token: Token?
+    public var token: Token {
+        get throws {
+            guard
+                let validToken = cached_token,
+                try !validToken.isExpired
+            else {
+                let newToken = try provider.token
+                cached_token = newToken
+                return newToken
+            }
+            return validToken
+        }
+    }
+    
+    public init(_ token: JWTToken) {
         self.provider = token
     }
 }
@@ -19,7 +34,17 @@ final class JWTRequestsAuthenticator {
 // MARK: - APIClientDelegate
 extension JWTRequestsAuthenticator: APIClientDelegate {
     
+    /// Adds the Authorization header with signed JWT Token.
     public func client(_ client: APIClient, willSendRequest request: inout URLRequest) async throws {
-        request.setValue("Bearer \(try provider.token)", forHTTPHeaderField: "Authorization")
+        request.setValue("Bearer \(try token)", forHTTPHeaderField: "Authorization")
+    }
+    
+    /// Retries 401 responses one more time in case token just expired.
+    public func client(_ client: APIClient, shouldRetry task: URLSessionTask, error: Error, attempts: Int) async throws -> Bool {
+        if case .unacceptableStatusCode(let statusCode) = error as? APIError,
+           statusCode == 401, attempts == 1 {
+            return true
+        }
+        return false
     }
 }
